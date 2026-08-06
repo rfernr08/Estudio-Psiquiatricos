34938e9984b100be3a270cfa6a75146eee18a6a9
Raúl <rfernr08@estudiantes.unileon.es>
2026-08-06 18:02:24 +0200

    Pruebas de vuelta de estudio de comunidades en grafos

--- code/neo4j/borrar_datos.py
+++ code/neo4j/borrar_datos.py
@@ -2,9 +2,6 @@ import dotenv
 import os
 from neo4j import GraphDatabase
 
-load_status = dotenv.load_dotenv("C:\\Users\\Usuario\\Documents\\Workspace\\Estudio-Psiquiatricos\\code\\neo4j\\Neo4j-921e6a7b-Created-2025-10-13.txt")
-if load_status is False:
-    raise RuntimeError('Environment variables not loaded.')
 
 URI = os.getenv("NEO4J_URI")
 AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
